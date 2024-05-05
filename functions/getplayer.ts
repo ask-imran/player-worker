43744afea7110627ce0e8a11787a814e45b1f210
askar-kani <>
2024-05-05 21:35:42 +1000

    add puppeteer and scrap a website

--- functions/getplayer.ts
+++ functions/getplayer.ts
@@ -1,5 +1,24 @@
-import type { Context } from "@netlify/functions"
+import type { Context } from "@netlify/functions";
+import puppeteer from "puppeteer";
 
+type Results = {
+    url: string;
+    text: string;
+}
 export default async (req: Request, context: Context) => {
-    return new Response("Hello, world!!!")
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://news.ycombinator.com/");
+    let results: Results[] = [];
+    await page.evaluate(() => {
+        let items = document.querySelectorAll('a.storylink');
+        items.forEach((item) => {
+            results.push({
+                url: item.getAttribute('href') || '',
+                text: item.textContent || '',
+            });
+        });
+    })
+    browser.close();
+    return new Response(JSON.stringify(results), { headers: { 'Content-Type': 'application/json' } });
 }
